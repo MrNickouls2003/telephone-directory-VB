204c76428e5a43e59d7640934e932cea4319d490
Nícolas Augusto Montovani <138739066+MrNickouls2003@users.noreply.github.com>
2024-09-26 19:03:34 -0300

    Add files via upload

---
+++ lista_telefonica/Form2.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        DataGridView1 = New DataGridView()
+        excluir_btn = New Button()
+        Button1 = New Button()
+        salvar_btn = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.BackgroundColor = SystemColors.ControlLightLight
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(55, 37)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(1063, 510)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' excluir_btn
+        ' 
+        excluir_btn.Font = New Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        excluir_btn.Location = New Point(1146, 509)
+        excluir_btn.Name = "excluir_btn"
+        excluir_btn.Size = New Size(100, 38)
+        excluir_btn.TabIndex = 1
+        excluir_btn.Text = "Excluir"
+        excluir_btn.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(1146, 468)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(100, 35)
+        Button1.TabIndex = 2
+        Button1.Text = "Voltar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' salvar_btn
+        ' 
+        salvar_btn.Font = New Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        salvar_btn.Location = New Point(1146, 422)
+        salvar_btn.Name = "salvar_btn"
+        salvar_btn.Size = New Size(100, 40)
+        salvar_btn.TabIndex = 3
+        salvar_btn.Text = "Salvar"
+        salvar_btn.UseVisualStyleBackColor = True
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(1272, 583)
+        Controls.Add(salvar_btn)
+        Controls.Add(Button1)
+        Controls.Add(excluir_btn)
+        Controls.Add(DataGridView1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form2"
+        Text = "Form2"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents excluir_btn As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents salvar_btn As Button
+End Class
